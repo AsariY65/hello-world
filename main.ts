3df57cdcbeebb7be868644549bd870e332dcb1e5
AsariY65 <122548025+AsariY65@users.noreply.github.com>
2023-01-13 11:25:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,8 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
 })
 basic.showIcon(IconNames.Sword)
+basic.forever(function () {
+    for (let index = 0; index < 500; index++) {
+        music.playMelody("E E E E E E E E ", 125)
+    }
+})
